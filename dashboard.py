b63694c104fb22ceee849d71ab30a918ef33dfd2
mifffffy <105280649+mifffffy@users.noreply.github.com>
2023-12-15 17:29:51 +0100

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import yfinance as yf
 from indicators import patterns
 import plotly.graph_objects as go
 import plotly.express as px
-import ta-lib-bin
+pip install ta-lib-bin
 
 option = st.sidebar.selectbox('Select Dashboard:', ('Technical', 'Social', 'Derivatives'))
 
